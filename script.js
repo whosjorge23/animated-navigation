fbb653f7f998f7a1c47e8c6dd4844e8e994b8454
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2020-08-07 10:52:21 +0200

    Refactor Javascript

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const nav2 = document.getElementById('nav-2')
 const nav3 = document.getElementById('nav-3')
 const nav4 = document.getElementById('nav-4')
 const nav5 = document.getElementById('nav-5')
+const navItems = [nav1, nav2, nav3, nav4, nav5]
+
+//Control Navigation Animation
+function navAnimation(directionRemove, directionAdd) {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${directionRemove}-${i + 1}`,`slide-${directionAdd}-${i + 1}`)
+    })
+}
 
 function toogleNav() {
     // Toggle: Menu Bars Open/Close
@@ -13,41 +21,19 @@ function toogleNav() {
     overlay.classList.toggle('overlay-active')
     if (overlay.classList.contains('overlay-active')){
         //Animate In - Overlay
-        overlay.classList.add('overlay-slide-right')
-        overlay.classList.remove('overlay-slide-left')
+        overlay.classList.replace('overlay-slide-down','overlay-slide-up')
         // Animate In - Nav Items
-        nav1.classList.add('slide-in-1')
-        nav1.classList.remove('slide-out-1')
-        nav2.classList.add('slide-in-2')
-        nav2.classList.remove('slide-out-2')
-        nav3.classList.add('slide-in-3')
-        nav3.classList.remove('slide-out-3')
-        nav4.classList.add('slide-in-4')
-        nav4.classList.remove('slide-out-4')
-        nav5.classList.add('slide-in-5')
-        nav5.classList.remove('slide-out-5')
+        navAnimation('out','in')
     }else{
         //Animate Out - Overlay
-        overlay.classList.remove('overlay-slide-right')
-        overlay.classList.add('overlay-slide-left')
+        overlay.classList.replace('overlay-slide-up','overlay-slide-down')
         //Animate Out - Nav Items
-        nav1.classList.remove('slide-in-1')
-        nav1.classList.add('slide-out-1')
-        nav2.classList.remove('slide-in-2')
-        nav2.classList.add('slide-out-2')
-        nav3.classList.remove('slide-in-3')
-        nav3.classList.add('slide-out-3')
-        nav4.classList.remove('slide-in-4')
-        nav4.classList.add('slide-out-4')
-        nav5.classList.remove('slide-in-5')
-        nav5.classList.add('slide-out-5')
+        navAnimation('in','out')
     }
 }
 
 //Event listeners
 menuBars.addEventListener('click',toogleNav)
-nav1.addEventListener('click',toogleNav)
-nav2.addEventListener('click',toogleNav)
-nav3.addEventListener('click',toogleNav)
-nav4.addEventListener('click',toogleNav)
-nav5.addEventListener('click',toogleNav)
+navItems.forEach((nav) => {
+    nav.addEventListener('click',toogleNav)
+})
